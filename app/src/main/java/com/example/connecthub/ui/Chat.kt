d5200802c6561ab07b1a5ec8094e9cf41727fc92
Gio-log <gierimaniak@gmail.com>
2025-06-06 20:11:30 +0200

    Added empty chat and Category_change activities and connected clickable content on Main_menu to them

---
+++ app/src/main/java/com/example/connecthub/ui/Chat.kt
@@ -0,0 +1,24 @@
+package com.example.connecthub.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.connecthub.R
+
+class Chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val access_token = intent.getStringExtra("access_token")
+        val token_type = intent.getStringExtra("token_type")
+        val login = intent.getStringExtra("login")
+    }
+}
